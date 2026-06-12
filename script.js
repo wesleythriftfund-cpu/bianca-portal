047aabc7297d77920a359723db6d960dc3e4b0c4
wesleythriftfund-cpu <wesleythriftfund@gmail.com>
2026-06-12 23:52:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
 function toggleCard(card)
 {
-card.classList.toggle("active");
-}
-
-function toggleAnniversary()
-{
-const popup =
-document.getElementById("anniversaryPopup");
-
-```
-if(popup.style.display === "flex")
-{
-    popup.style.display = "none";
-}
-else
-{
-    popup.style.display = "flex";
-}
-```
-
+    card.classList.toggle("active");
 }
