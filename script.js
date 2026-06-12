4e4ad4d8b3af8198937db3a13908c731c67cc746
wesleythriftfund-cpu <wesleythriftfund@gmail.com>
2026-06-13 00:05:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ function toggleCard(card)
     card.classList.toggle("active");
 }
 
-const anniversaryDate = new Date("2026-05-16");
-
-const today = new Date();
-
-const difference = today - anniversaryDate;
-
-const days = Math.floor(
-    difference / (1000 * 60 * 60 * 24)
-);
-
-document.addEventListener("DOMContentLoaded", function()
+window.onload = function()
 {
+    const anniversaryDate = new Date("2026-05-16");
+
+    const today = new Date();
+
+    const difference = today - anniversaryDate;
+
+    const days = Math.floor(
+        difference / (1000 * 60 * 60 * 24)
+    );
+
     const daysElement =
     document.getElementById("daysTogether");
 
     if(daysElement)
     {
         daysElement.innerHTML =
-        days + " Days ❤️";
+        days + " days ❤️";
     }
-});
+};
